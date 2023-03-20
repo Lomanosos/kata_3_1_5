8f065733f1f4a8ca58be204a40cc647a0c218cfa
Sonya <lomanosos8@gmail.com>
2023-03-20 18:50:36 +0300

    done, needs a few changes 2.0

--- src/main/java/ru/kata/spring/boot_security/demo/service/TableInserter.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/TableInserter.java
@@ -6,16 +6,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 
 @Component
 public class TableInserter implements CommandLineRunner {
-    private UserRepository userRepository;
+
     private PasswordEncoder passwordEncoder;
+    private UserAndRoleService userAndRoleService;
     @Autowired
-    public TableInserter(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
+    public TableInserter(UserAndRoleService userAndRoleService, PasswordEncoder passwordEncoder) {
+        this.userAndRoleService = userAndRoleService;
         this.passwordEncoder = passwordEncoder;
+
     }
 
     @Override
@@ -26,12 +29,16 @@ public class TableInserter implements CommandLineRunner {
                 passwordEncoder.encode("qwerty66"), 15);
         User user3 = new User("logitec",
                 passwordEncoder.encode("thebestcompany"), 82);
-        user1.addRole(new Role("ROLE_USER"));
-        user2.addRole(new Role("ROLE_ADMIN"));
-        user3.addRole(new Role("ROLE_USER"));
-        user3.addRole(new Role("ROLE_ADMIN"));
-        userRepository.save(user1);
-        userRepository.save(user2);
-        userRepository.save(user3);
+        Role user = new Role("USER");
+        Role admin = new Role("ADMIN");
+        userAndRoleService.saveRole(user);
+        userAndRoleService.saveRole(admin);
+        user1.addRole(user);
+        user2.addRole(admin);
+        user3.addRole(admin);
+        user3.addRole(user);
+        userAndRoleService.saveUser(user1);
+        userAndRoleService.saveUser(user2);;
+        userAndRoleService.saveUser(user3);
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/service/UserAndRoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserAndRoleService.java
@@ -39,6 +39,10 @@ public class UserAndRoleService implements UserDetailsService {
         userRepository.save(user);
     }
     @Transactional
+    public void saveRole(Role role) {
+        roleRepository.save(role);
+    }
+    @Transactional
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
